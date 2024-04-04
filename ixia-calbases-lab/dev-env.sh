2ac09f3983560a2fbd8fea3a6c90a53de1109e11
selldinesh <dinesh.sellappan@keysight.com>
2024-04-04 18:25:34 +0000

    cleanup

--- ixia-calbases-lab/dev-env.sh
+++ ixia-calbases-lab/dev-env.sh
@@ -1,22 +1,13 @@
 SCRIPT_DIR=`pwd`
-DATE_TIME=`date | sed 's/ //g' | sed 's/://g'`
 sudo chown -R AzDevOps:gAzDevOps ~/sonic-mgmt
-#cp -rf ~/sonic-mgmt  ~/sonic-mgmt-bkp-$DATE_TIME
-# sudo rm -f  ~/sonic-mgmt/ansible/group_vars/all/inv_mapping.yml
-# sudo rm -f  ~/sonic-mgmt/ansible/group_vars/all/inv_mapping.json
-# sudo rm -f  ~/sonic-mgmt/ansible/group_vars/lab/lab.yml
+
 sudo rm -f  ~/sonic-mgmt/ansible/testbed.csv
-# sudo rm -f  ~/sonic-mgmt/ansible/files/snappi_sonic_connection_graph.xml
 sudo rm -f  ~/sonic-mgmt/ansible/files/sonic_snappi-sonic_devices.csv
 sudo rm -f  ~/sonic-mgmt/ansible/files/sonic_snappi-sonic_links.csv
 sudo rm -rf ~/sonic-mgmt/ansible/group_vars/snappi-sonic/
 sudo rm -f  ~/sonic-mgmt/ansible/snappi-sonic
 
-# cp -f  $SCRIPT_DIR/ansible/group_vars/all/inv_mapping.yml ~/sonic-mgmt/ansible/group_vars/all/inv_mapping.yml
-# cp -f  $SCRIPT_DIR/ansible/group_vars/all/inv_mapping.json  ~/sonic-mgmt/ansible/group_vars/all/inv_mapping.json
-# cp -f  $SCRIPT_DIR/ansible/group_vars/lab/lab.yml ~/sonic-mgmt/ansible/group_vars/lab/lab.yml
 cp -f  $SCRIPT_DIR/ansible/testbed.csv ~/sonic-mgmt/ansible/testbed.csv
-# cp -f  $SCRIPT_DIR/ansible/files/snappi_sonic_connection_graph.xml ~/sonic-mgmt/ansible/files/snappi_sonic_connection_graph.xml
 cp -f  $SCRIPT_DIR/ansible/files/sonic_snappi-sonic_links.csv ~/sonic-mgmt/ansible/files/sonic_snappi-sonic_links.csv
 cp -f  $SCRIPT_DIR/ansible/files/sonic_snappi-sonic_devices.csv ~/sonic-mgmt/ansible/files/sonic_snappi-sonic_devices.csv
 cp -rf $SCRIPT_DIR/ansible/group_vars/snappi-sonic/ ~/sonic-mgmt/ansible/group_vars/snappi-sonic/
