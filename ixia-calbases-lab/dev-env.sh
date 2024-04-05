5fc7f2581e807935ba8335ad8f1cb8b8181d7989
selldinesh <dinesh.sellappan@keysight.com>
2024-04-05 22:14:54 +0000

    updated files

--- ixia-calbases-lab/dev-env.sh
+++ ixia-calbases-lab/dev-env.sh
@@ -14,7 +14,8 @@ if [ "$1" = "Novus" ] || [ "$1" = "novus" ]; then
     cp -f  $SCRIPT_DIR/Novus/ansible/testbed.csv  /var/AzDevOps/sonic-mgmt/ansible/testbed.csv
     cp -f  $SCRIPT_DIR/Novus/ansible/files/sonic_snappi-sonic_links.csv  /var/AzDevOps/sonic-mgmt/ansible/files/sonic_snappi-sonic_links.csv
     cp -f  $SCRIPT_DIR/Novus/ansible/files/sonic_snappi-sonic_devices.csv  /var/AzDevOps/sonic-mgmt/ansible/files/sonic_snappi-sonic_devices.csv
-    cp -rf  $SCRIPT_DIR/Novus/ansible/group_vars/snappi-sonic/  /var/AzDevOps/sonic-mgmt/ansible/group_vars/snappi-sonic/
+    cp -f  $SCRIPT_DIR/Novus/ansible/group_vars/snappi-sonic/snappi-sonic.yml  /var/AzDevOps/sonic-mgmt/ansible/group_vars/snappi-sonic/snappi-sonic.yml
+    cp -f  $SCRIPT_DIR/Novus/ansible/group_vars/snappi-sonic/secrets.yml  /var/AzDevOps/sonic-mgmt/ansible/group_vars/snappi-sonic/secrets.yml
     cp -f  $SCRIPT_DIR/Novus/ansible/snappi-sonic  /var/AzDevOps/sonic-mgmt/ansible/snappi-sonic
 
 elif [ "$1" = "AresOne" ] || [ "$1" = "aresone" ]; then
@@ -22,7 +23,8 @@ elif [ "$1" = "AresOne" ] || [ "$1" = "aresone" ]; then
     cp -f  $SCRIPT_DIR/AresOne/ansible/testbed.csv  /var/AzDevOps/sonic-mgmt/ansible/testbed.csv
     cp -f  $SCRIPT_DIR/AresOne/ansible/files/sonic_snappi-sonic_links.csv  /var/AzDevOps/sonic-mgmt/ansible/files/sonic_snappi-sonic_links.csv
     cp -f  $SCRIPT_DIR/AresOne/ansible/files/sonic_snappi-sonic_devices.csv  /var/AzDevOps/sonic-mgmt/ansible/files/sonic_snappi-sonic_devices.csv
-    cp -rf  $SCRIPT_DIR/AresOne/ansible/group_vars/snappi-sonic/  /var/AzDevOps/sonic-mgmt/ansible/group_vars/snappi-sonic/
+    cp -f  $SCRIPT_DIR/AresOne/ansible/group_vars/snappi-sonic/snappi-sonic.yml  /var/AzDevOps/sonic-mgmt/ansible/group_vars/snappi-sonic/snappi-sonic.yml
+    cp -f  $SCRIPT_DIR/AresOne/ansible/group_vars/snappi-sonic/secrets.yml  /var/AzDevOps/sonic-mgmt/ansible/group_vars/snappi-sonic/secrets.yml
     cp -f  $SCRIPT_DIR/AresOne/ansible/snappi-sonic  /var/AzDevOps/sonic-mgmt/ansible/snappi-sonic
 else
     echo "Invalid Card Type"
